c6c28fb537cd853c8e019dcafee90c42c2e71c7f
davisstacked <brettzvi@gmail.com>
2021-01-21 13:57:09 -0800

    got axios and completed an external API request to Github.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React, { Fragment, Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
+
 class App extends Component {
+
+  state = {
+    users: [],
+    loading: false,
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true })
+    // Make a request to Github API 
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false })
+  }
 
   render() {
 
@@ -11,7 +26,7 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className="container">
-        <Users />
+        <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
