b93d99f7b0a0bc52a4eea415be56b5d265a722ef
davisstacked <brettzvi@gmail.com>
2021-01-21 09:01:31 -0800

    created react-app and Navbar component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Fragment, Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className='App'>
+        <Navbar />
+      </div>
+    );
+
+  }
+}
+
+export default App;
