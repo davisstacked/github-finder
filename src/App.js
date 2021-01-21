47e9a50c08f1dce73fe51036f267d6b6341080c3
davisstacked <brettzvi@gmail.com>
2021-01-21 09:30:26 -0800

    Created UserItem JS and addedstate to the component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className='App'>
         <Navbar />
+        <UserItem />
       </div>
     );
 
