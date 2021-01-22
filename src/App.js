3911f2c07466dc3aae52d7e08053855818cd09da
davisstacked <brettzvi@gmail.com>
2021-01-22 11:03:21 -0800

    added clear button

--- src/App.js
+++ src/App.js
@@ -23,13 +23,17 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   }
 
+  // Clear users from state
+
+  clearUsers = () => this.setState({ users: [], loading: false  })
+
   render() {
 
     return (
       <div className='App'>
         <Navbar />
         <div className="container">
-        <Search searchUsers={this.searchUsers} />
+        <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={ this.state.users.length > 0 ? true: false }/>
         <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -9,6 +9,8 @@ export class Search extends Component {
 
     static propTypes = {
         searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
     };
 
     onSubmit = e => {
@@ -38,6 +40,11 @@ export class Search extends Component {
                     className="btn btn-dark btn-block"
                 />
                 </form>
+                { this.props.showClear && <button 
+                    className="btn btn-light btn-block" 
+                    onClick={this.props.clearUsers}> Clear
+                </button>
+                }
             </div>
         )
     }
