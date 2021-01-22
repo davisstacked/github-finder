9734e7684ef7226c9d917b5dd05a5d1bfcae57c7
davisstacked <brettzvi@gmail.com>
2021-01-22 15:26:12 -0800

    added react-router-dom and an about page

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
 
         return (
             <nav className="navbar bg-primary">
                 <h1>
-                    <i className={icon} />{title}
+                    <i className={icon} /> {title}
                 </h1>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
             </nav>
         )
 }
