2c8137b5ad3c7307e4f5bd6b77f32ee8e54298ec
davisstacked <brettzvi@gmail.com>
2021-01-23 13:49:14 -0800

    converted user.js to react hooks

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -4,10 +4,6 @@ import PropTypes from 'prop-types';
 const Search = ({ searchUsers, showClear, clearUsers, setAlert  }) => {
     const [text, setText] = useState('');
 
-    state = {
-        text: '' 
-    };
-
     const onSubmit = e => {
         e.preventDefault();
         if(text === '') {
@@ -22,31 +18,31 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert  }) => {
     const onChange = e => setText(e.target.value);
 
 
-        return ( 
-            <div>
-                <form onSubmit={onSubmit} className="form">
+    return ( 
+        <div>
+            <form onSubmit={onSubmit} className="form">
 
-                <input 
-                    type="text"  
-                    name="text" 
-                    placeholder="Search Users..."
-                    value={text}
-                    onChange={onChange}
-                />
-                <input 
-                    type="submit"  
-                    value="Search" 
-                    className="btn btn-dark btn-block"
-                />
-                </form>
-                { showClear && <button 
-                    className="btn btn-light btn-block" 
-                    onClick={clearUsers}> Clear
-                </button>
-                }
-            </div>
-        )
-    
+            <input 
+                type="text"  
+                name="text" 
+                placeholder="Search Users..."
+                value={text}
+                onChange={onChange}
+            />
+            <input 
+                type="submit"  
+                value="Search" 
+                className="btn btn-dark btn-block"
+            />
+            </form>
+            { showClear && <button 
+                className="btn btn-light btn-block" 
+                onClick={clearUsers}> Clear
+            </button>
+            }
+        </div>
+    )
+
 }
 
 Search.propTypes = {
