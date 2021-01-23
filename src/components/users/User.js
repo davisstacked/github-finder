0f97dd5e6a661982dc43460b009bef4f2f46a697
davisstacked <brettzvi@gmail.com>
2021-01-23 08:19:25 -0800

    added hireable, name, photo and location to user profile page

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react'
+import React, { Fragment, Component } from 'react';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login);
     }
+
+    static propTypes = {
+        loading: PropTypes.bool,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired,
+    }
+
     render() {
         const {
             name,
@@ -22,10 +32,30 @@ export class User extends Component {
 
         const { loading } = this.props;
 
+        if(loading) return <Spinner />
+
         return (
-            <div>
-                {name}    
-            </div>
+            <Fragment>
+                <Link to='/' className='btn btn-light'>
+                    Back To Search
+                </Link>
+                Hireable: { ' ' }
+                {hireable ? (
+                    <i className="fas fa-check text-success" /> ) : ( <i className="fas fa-times-circle text-danger" /> 
+                )} 
+                <div className="card grid-2">
+                    <div className="all-center">
+                    <img 
+                        src={avatar_url} 
+                        className="round-img" 
+                        alt="" 
+                        style={{ width: '150px' }}
+                    />
+                    <h1>{name}</h1>
+                    <p>Location: {location}</p>
+                    </div>
+                </div>
+            </Fragment>
         )
     }
 }
