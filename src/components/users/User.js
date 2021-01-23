668528b71af4613d83d4fdf828817ffff1e20194
davisstacked <brettzvi@gmail.com>
2021-01-23 08:53:37 -0800

    built out user profile

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -23,6 +23,7 @@ export class User extends Component {
             blog,
             login,
             html_url,
+            company,
             followers,
             following,
             public_repos,
@@ -54,9 +55,44 @@ export class User extends Component {
                     <h1>{name}</h1>
                     <p>Location: {location}</p>
                     </div>
+                    <div>
+                    {bio && (
+                        <Fragment>
+                            <h3>Bio</h3>
+                            <p>{bio}</p>
+                        </Fragment>
+                    )} 
+                    <a href={html_url} className="btn btn-dark my-1">
+                        Visit Github Profile
+                    </a>
+                    <ul>
+                        <li>
+                            {login && <Fragment>
+                                <strong>Username: </strong> {login}
+                            </Fragment>}
+                        </li>
+                        <li>
+                            {company && <Fragment>
+                                <strong>Company: </strong> {company}
+                            </Fragment>}
+                        </li>
+                        <li>
+                            {blog && <Fragment>
+                                <strong>Website: </strong> {blog}
+                            </Fragment>}
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+
+                <div className="card text-center">
+                    <div className="badge badge-primary">Followers: {followers}</div>
+                    <div className="badge badge-success">Following: {following}</div>
+                    <div className="badge badge-danger">Public Repos: {public_repos}</div>
+                    <div className="badge badge-dark">Public Gists: {public_gists}</div>
                 </div>
             </Fragment>
-        )
+        );
     }
 }
 
