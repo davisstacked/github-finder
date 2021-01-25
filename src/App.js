6994fa044fe86dd3f51ac9cf2ff33d0b641f5a24
davisstacked <brettzvi@gmail.com>
2021-01-25 14:27:53 -0800

    added a netlify.toml file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
