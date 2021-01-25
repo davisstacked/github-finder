c638757b19d1cacdfcdf9910cf719d026e364785
davisstacked <brettzvi@gmail.com>
2021-01-25 08:55:37 -0800

    converted repos to context api

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
-import axios from 'axios';
 
 import GithubState from './context/github/GithubState';
 
@@ -19,19 +18,6 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  // Get user's repos
-  const getUserRepos = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=$
-    {
-      process.env.REACT_APP_GITHUB_CLIENT_ID
-    }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setRepos(res.data);
-    setLoading(false);
-  };
 
   // Set Alert 
   const showAlert = (msg, type) => {
@@ -64,12 +50,7 @@ const App = () => {
             <Route 
               exact 
               path='/user/:login' 
-              render={props => (
-              <User 
-                { ...props } 
-                getUserRepos={getUserRepos} 
-                repos={repos}
-              />
+              component = {User}
             )} 
             />
           </Switch>
