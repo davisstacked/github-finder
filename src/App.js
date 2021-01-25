d370561ca113c713b809d71940f6532d66a270d1
davisstacked <brettzvi@gmail.com>
2021-01-25 13:54:28 -0800

    created Home.js for routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
+import Home from './components/pages/Home';
 
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
@@ -14,9 +13,6 @@ import './App.css';
 
 
 const App = () => {
- 
-  const [alert, setAlert] = useState(null);
-
 
   return (
     <GithubState>
@@ -27,21 +23,9 @@ const App = () => {
             <div className="container">
             <Alert />
             <Switch>
-              <Route 
-                exact path='/' 
-                render={props => (
-                <Fragment>  
-                  <Search />
-                  <Users />
-                </Fragment>
-                )} 
-              />
+              <Route exact path='/' component={Home} />
               <Route exact path='/about' component={About} />
-              <Route 
-                exact 
-                path='/user/:login' 
-                component = {User}
-              />
+              <Route exact path='/user/:login' component ={User} />
             </Switch>
             </div>
           </div>
